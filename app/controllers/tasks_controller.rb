4e9ecd17f51ff2c3bb9f6b264e89f5c4d329469f
unknown <DarrylW@DARRYLW-PB.datacom.co.nz>
2013-02-02 10:53:07 +1300

    First commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index]
+  load_and_authorize_resource
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    if @task.save
+      redirect_to @task, :notice => "Successfully created task."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(params[:task])
+      redirect_to @task, :notice  => "Successfully updated task."
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, :notice => "Successfully destroyed task."
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandbox2::Application.config.secret_token = 'a9ec303741b11f651749e1d5ec6cd9b90771b07a4a197e0d0f5c70d7e92007fae143feede9f04659987c9aa03aadb0ab55de733db9e7cc80a75d3f88b3fe3df5'
